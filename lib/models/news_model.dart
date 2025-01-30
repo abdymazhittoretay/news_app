bd242d983ec685c0f5ec8f1e0e07c61b5da32dcb
Toretay <abdymvzhit@gmail.com>
2025-01-30 18:16:17 +0500

    added news_model and news list to store news

---
+++ lib/models/news_model.dart
@@ -0,0 +1,13 @@
+class NewsModel {
+  final String title;
+  final String url;
+  final String urlToImage;
+  final String publishedAt;
+
+  NewsModel({
+    required this.title,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+  });
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:news_app/models/news_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,13 +14,22 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final String _apiKey = dotenv.env["API_KEY"] ?? "";
+  final List<NewsModel> _news = [];
   Future _getNews() async {
     try {
       var response = await http.get(Uri.https("newsapi.org", "/v2/everything", {
         "q": "bitcoin",
         "apiKey": _apiKey,
       }));
-      print(response.body);
+      var data = jsonDecode(response.body);
+      for (var i in data["articles"]) {
+        _news.add(NewsModel(
+            title: i["title"] ?? "",
+            url: i["url"] ?? "",
+            urlToImage: i["urlToImage"] ?? "",
+            publishedAt: i["publishedAt"] ?? ""));
+      }
+      print(_news.length);
     } catch (e) {
       print("Error with getting the news $e");
     }
